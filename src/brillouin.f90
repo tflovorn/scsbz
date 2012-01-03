e586ad1201b68e2ec20c7041f9dca9e0180e7f63
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-01-03 15:22:07 -0600

    brillouin sumFunc depends on env

--- src/brillouin.f90
+++ src/brillouin.f90
@@ -1,14 +1,16 @@
 module brillouin
+    use double
+    use environment
 contains
     function BrilSum(env, sumFunc)
         ! uses Kahan summation algorithm to improve accuracy
-        use double
-        use environment
         implicit none
         type(Environ), intent(in) :: env
         interface
-            function sumFunc(k)
+            function sumFunc(env, k)
                 use double
+                use environment
+                type(Environ), intent(in) :: env
                 real(kind=DP), intent(in) :: k(1:3)
                 real(kind=DP) :: sumFunc
             end function sumFunc
@@ -24,7 +26,7 @@ contains
         xLoop: do nx=1, env%zoneLength
             yLoop: do ny=1, env%zoneLength
                 zLoop: do nz=1, env%zoneLength
-                    y = sumFunc(k) - c
+                    y = sumFunc(env, k) - c
                     t = BrilSum + y
                     c = (t - BrilSum) - y
                     BrilSum = t
