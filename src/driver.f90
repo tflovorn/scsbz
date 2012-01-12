990697938c3fa66c0e72a5d7bb6b13301ac1d653
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-01-11 18:18:29 -0600

    add tc = 0 first run to driver

--- src/driver.f90
+++ src/driver.f90
@@ -23,11 +23,22 @@ contains
         open(outputUnit, file=outputFileName, recl=recLen)
         read (inputUnit, *) runCount
         write (outputUnit, *) runCount
+        sbzRun = 0
         do i=1,runCount
             read (inputUnit, *) zoneLength, t, tc, beta, x, J
-            env = NewEnv(zoneLength, t, tc, beta, x, J)
-            ! TODO: can get better starting values here by first solving the
-            ! reduced system with tc = 0.
+            ! Start with a system with no c-direction hopping to get reasonable
+            ! initial values for system with tc > 0.
+            env = NewEnv(zoneLength, t, 0.0_DP, beta, x, J)
+            system = sbzSystemNoC(env, toleranceValues)
+            error = scSystemSolve(system, env)
+            if (error > 0) then
+                ! Quit at the first error.
+                ! May instead want to continue on here.
+                sbzRun = error
+                exit
+            end if
+            ! Done with tc = 0 approximation. Move on to real system.
+            env%tc = tc
             system = sbzSystem(env, toleranceValues)
             error = scSystemSolve(system, env)
             if (error > 0) then
@@ -35,9 +46,7 @@ contains
                 ! May instead want to continue on here, and write error
                 ! indicators on the appropriate lines of the output file.
                 sbzRun = error
-                close (inputUnit)
-                close (outputUnit)
-                return
+                exit
             end if
             write (outputUnit, *) env%muF, env%muB, env%D, env%B, env%A
             write (outputUnit, *) env%Dc, env%Bc, env%Ac
